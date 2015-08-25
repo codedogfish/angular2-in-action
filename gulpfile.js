3194d89b098a2d22a00894897968343d8f6a3755
codedogfish <jackcool1989@outlook.com>
2015-08-25 14:41:13 +0800

    Init repo

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('serve',function(){
+	browserSync({
+		server:{
+			baseDir:'.'
+		}
+	});
+	gulp.watch('./**/*.*',browserSync.reload);
+})
